6e22406a018af1531147a30439a1ef2abd39cc7a
Hardik <hardikvu0204@gmail.com>
2020-03-18 22:57:55 +0530

    wrap up added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	before_action :set_article, only: [ :edit, :update, :destroy, :show ]
+
 	def index
 		@articles = Article.all	
 	end
@@ -19,15 +21,12 @@ class ArticlesController < ApplicationController
   end
 
 	def show 
-		@article = Article.find(params[:id])
 	end
 
 	def edit
-		@article = Article.find(params[:id])	
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully updated"
 			redirect_to article_path(@article)
@@ -37,13 +36,15 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 		flash[:notice] = "Article is successfully deleted"
 		redirect_to articles_path
 	end
 
   private
+	def set_article
+		@article = Article.find(params[:id])
+	end
   def article_params
     params.require(:article).permit(:title,:description)
   end
