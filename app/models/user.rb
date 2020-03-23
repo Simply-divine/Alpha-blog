491afb4377a9c5d2c8782de86f8bff502afec9da
Hardik <hardikvu0204@gmail.com>
2020-03-23 18:32:12 +0530

    User display added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_many :articles
 	before_save {self.email = email.downcase}
-	validates :username, presence:true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 5} 
+	validates :username, presence:true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 10} 
 	VALID_EMAIL_REGEX= /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, 
 						length: {maximum: 105}, 
