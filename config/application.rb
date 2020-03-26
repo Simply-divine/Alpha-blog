285766a5a7a5ca1a37ae7bfba6d4bb45e74b4138
Hardik <hardikvu0204@gmail.com>
2020-03-26 12:21:40 +0530

    precompile issue solve

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module AlphaBlog
-  cllass Application < Rails::Application
+  class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     # config.assets.initialize_on_precompile = false
